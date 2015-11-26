bbb33a8ce7f74d754ae5db15f0de37e3221bb308
Daniel Rasmuson <dan123911@gmail.com>
2015-11-26 12:10:55 -0800

    forward progress

--- data/dronedeploy/meshConverter.js
+++ data/dronedeploy/meshConverter.js
@@ -47,7 +47,8 @@ var reconstruction = {
 // # Label,X/East,Y/North,Z/Altitude,Yaw,Pitch,Roll,Error (m),X error,Y error,Z error,Error (deg),Yaw error,Pitch error,Roll error,X est,Y est,Z est,Yaw est,Pitch est,Roll est
 // dji_0644.jpg,-123.114647,38.426791,91.000000,,,,1.967148,-1.217930,1.488887,-0.411745,,,,,-123.114661,38.426805,90.588254,9.331940,1.245151,0.161194
 // [['shot name', 'yaw', 'pitch', 'roll', 'unknown', 'unknown', 'unknown', 'lat', 'lon', 'alt'],['dji_0644.jpg', ]]
-function addShot(shotName, camera){
+function newShot(shotName){
+  // this.cameraName
 }
 
 function addPoint(){
@@ -61,12 +62,12 @@ function addPoint(){
 //   k2: 0.014685481212948845,
 //   width: 4000
 // }
-function addCamera(name, cameraSettings){
-  // TODO understand 
-  // reconstructionMeshed can contain an array of recons
-  reconstruction.cameras[name] = cameraSettings;
+function addCamera(cameraName, cameraSettings){
+  var camera = this;
+  camera.cameraName = cameraName;
+  reconstruction.cameras[cameraName] = cameraSettings;
   return {
-    addShot: addShot 
+    newShot: newShot.bind(camera) 
   }
 }
 
